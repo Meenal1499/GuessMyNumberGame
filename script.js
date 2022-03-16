2c20a09f0a15ff9a2a7c72ceaa979a0b677bac5b
Meenal1499 <meenal01.0499@gmail.com>
2022-03-16 18:53:08 +0530

    Second commit

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 'use strict';
-console.log(document.querySelector('.message').textContent);
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '🚫 No number!';
+  }
+});
