b44e4aa3e1141d5f86a75fbe68c066867c4a534d
Meenal1499 <meenal01.0499@gmail.com>
2022-03-16 19:44:52 +0530

    All number case commit

--- script.js
+++ script.js
@@ -1,19 +1,44 @@
 'use strict';
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
 document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess);
 
+  //when there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '🚫 No Number!';
-  } else if (guess === secretNumber) {
+  }
+
+  //when player wins
+  else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
-  } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = '📈 Too High!';
-  } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = '📉 Too Low!';
+  }
+
+  //when number is too high
+  else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too High!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You Lost The Game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+
+  //when number is too low
+  else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too Low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You Lost The Game!';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
